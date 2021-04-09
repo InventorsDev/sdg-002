c1a4e7fe3a8b5efdf2cbd1a181e479d7a8f7a342
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-09 10:15:35 +0100

    created signup-success page

---
+++ pages/signup-success.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-custom">
+    &nbsp;
+    <b-container>
+      <!-- Glassmorphism -->
+      <div class="glass text-center mt-20vh mb-5">
+        <h1 class="font-weight-bold">
+          Sign up
+          <br />
+          <span class="text-lemon-100">Successfull</span>
+        </h1>
+        <div class="mt-5 mb-5">
+          <img
+            src="~/assets/images/ok.png"
+            class="img-fluid"
+            alt="Illustration"
+          />
+          <p class="mt-4">
+            Please
+            <a href="" class="text-uppercase text-lemon-100">Login</a>
+          </p>
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
