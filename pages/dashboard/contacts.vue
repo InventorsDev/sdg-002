beac5365b60505a177847ea9f25f25d84ad03c93
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-09 16:49:42 +0100

    new updates

---
+++ pages/dashboard/contacts.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="container">
+      <h1 class="mt-5 text-green-400 font-weight-bolder">Life <br />savers</h1>
+      <section>
+        <h5 class="mt-4 text-green-400 fw-600">Recent</h5>
+        <div class="mt-5">
+          <b-row>
+            <b-col cols="auto">
+              <b-avatar variant="info" text="A" size="lg"></b-avatar>
+            </b-col>
+            <b-col cols="auto pl-2">
+              <h5 class="text-green-200">Elijah</h5>
+              <p class="text-muted">2 days ago</p>
+            </b-col>
+          </b-row>
+          <b-row class="mt-3">
+            <b-col cols="auto">
+              <b-avatar text="BA" size="lg"></b-avatar>
+            </b-col>
+            <b-col cols="auto pl-2">
+              <h5 class="text-green-200">Elijah</h5>
+              <p class="text-muted">2 days ago</p>
+            </b-col>
+          </b-row>
+        </div>
+      </section>
+      <!-- ... -->
+      <section class="mt-5 mb-5">
+        <h4 class="text-green-400 d-inline-block fw-600">
+          Later <span class="h5"><BIconChevronRight /></span>
+        </h4>
+
+        <b-row class="mt-4 flex-nowrap overflow-auto">
+          <b-col cols="auto">
+            <b-avatar
+              class="border-0"
+              variant="warning"
+              text="Ae"
+              size="6rem"
+            ></b-avatar>
+          </b-col>
+          <b-col cols="auto">
+            <b-avatar
+              class="border-0"
+              variant="success"
+              text="Ae"
+              size="6rem"
+            ></b-avatar>
+          </b-col>
+          <b-col cols="auto">
+            <b-avatar
+              class="border-0"
+              variant="info"
+              text="Ae"
+              size="6rem"
+            ></b-avatar>
+          </b-col>
+          <b-col cols="auto">
+            <b-avatar class="border-0" text="Ae" size="6rem"></b-avatar>
+          </b-col>
+        </b-row>
+      </section>
+    </div>
+    <bottom-nav />
+  </div>
+</template>
+<script>
+import { BIconChevronRight } from 'bootstrap-vue'
+export default {
+  components: {
+    BIconChevronRight,
+  },
+  transition: 'fade',
+}
+</script>
