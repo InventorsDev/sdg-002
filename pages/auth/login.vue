8722a47de4972dabc678326913cc10036bb9a083
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-17 23:24:43 +0100

    connected auth pages to back-end

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -9,14 +9,14 @@
           <br />
           <span class="text-lemon-100">welcome</span>
         </h1>
-        <b-form class="mt-5">
+        <b-form class="mt-5" @submit.prevent="login">
           <b-form-group>
-            <material-input-1 type="text">
-              <span class="text-white text-uppercase">Username</span>
+            <material-input-1 type="email" v-model="user.email">
+              <span class="text-white text-uppercase">Email Address</span>
             </material-input-1>
           </b-form-group>
           <b-form-group class="mt-5">
-            <material-input-1 type="password">
+            <material-input-1 type="password" v-model="user.password">
               <span class="text-white text-uppercase">Password</span>
             </material-input-1>
           </b-form-group>
@@ -26,12 +26,16 @@
           </div>
           <div class="text-center mt-5 mb-5">
             <b-button
+              type="submit"
               size="lg"
               class="bg-green-gradient border-0 text-white rounded-pill w-100"
             >
               LOGIN
             </b-button>
-            <b-link to="/auth/signup" class="mt-3 text-white mt-2 d-inline-block">
+            <b-link
+              to="/auth/signup"
+              class="mt-3 text-white mt-2 d-inline-block"
+            >
               SIGN UP
             </b-link>
           </div>
@@ -43,5 +47,41 @@
 <script>
 export default {
   layout: 'no-auth',
+  data() {
+    return {
+      user: {
+        email: null,
+        password: null,
+      },
+    }
+  },
+  methods: {
+    async login() {
+      this.$nuxt.$loading.start()
+      await this.$axios
+        .$post('/auth/login', this.user)
+        .then((res) => {
+          this.$nuxt.$loading.finish()
+          console.log(res)
+          if (res.token) {
+            localStorage.user_token = res.token
+            this.$router.push('/')
+          } else {
+            // ...
+          }
+        })
+        .catch((err) => {
+          let errors = err.response.data.errors
+          let fError = errors[Object.keys(errors)[0]][0]
+          this.$bvToast.toast(fError, {
+            title: 'Registeration Error!',
+            toaster: 'b-toaster-bottom-center',
+            solid: true,
+            variant: 'danger',
+            appendToast: true,
+          })
+        })
+    },
+  },
 }
 </script>
--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -10,27 +10,28 @@
           <span class="text-lemon-100">Sign Up</span>
         </h1>
         <!-- .. -->
-        <b-form class="mt-5">
+        <b-form class="mt-5" @submit.prevent="createAccount">
           <b-form-group>
-            <material-input-1 type="text">
+            <material-input-1 type="text" v-model="user.name">
               <span class="text-white text-uppercase">Full name</span>
             </material-input-1>
           </b-form-group>
           <!-- .. -->
           <b-form-group class="mt-4 py-2">
-            <material-input-1 type="email">
+            <material-input-1 type="email" v-model="user.email">
               <span class="text-white text-uppercase">Email</span>
             </material-input-1>
           </b-form-group>
           <!-- .. -->
           <b-form-group class="mt-4 py-2">
-            <material-input-1 type="password">
+            <material-input-1 type="password" v-model="user.password">
               <span class="text-white text-uppercase">Password</span>
             </material-input-1>
           </b-form-group>
           <!-- .. -->
           <div class="text-center mt-5 mb-5">
             <b-button
+              type="submit"
               size="lg"
               class="bg-green-gradient border-0 text-white rounded-pill w-100"
             >
@@ -49,7 +50,58 @@
   </div>
 </template>
 <script>
+import stringify from 'qs/lib/stringify'
+import { componentsPlugin } from 'bootstrap-vue'
 export default {
   layout: 'no-auth',
+  data() {
+    return {
+      user: {
+        name: null,
+        email: null,
+        password: null,
+      },
+    }
+  },
+  methods: {
+    async createAccount() {
+      const self = this
+      // ...
+      if (this.user.password.length < 7) {
+        this.$bvToast.toast('Password must be greater than 6 characters', {
+          title: 'Registeration Error!',
+          toaster: 'b-toaster-bottom-center',
+          solid: true,
+          variant: 'danger',
+          appendToast: true,
+        })
+      } else {
+        // ...
+        this.$nuxt.$loading.start()
+        await this.$axios
+          .$post('/auth/register', this.user)
+          .then((res) => {
+            this.$nuxt.$loading.finish()
+            if (res.token) {
+              localStorage.user_token = res.token
+              this.$router.push('/')
+            } else {
+              // ...
+            }
+          })
+          .catch((err) => {
+            let errors = err.response.data.errors
+            let fError = errors[Object.keys(errors)[0]][0]
+            this.$bvToast.toast(fError, {
+              title: 'Registeration Error!',
+              toaster: 'b-toaster-bottom-center',
+              solid: true,
+              variant: 'danger',
+              appendToast: true,
+            })
+          })
+      }
+    },
+  },
 }
 </script>
