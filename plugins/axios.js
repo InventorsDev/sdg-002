aff567adac5dba90783c47376311dca53248976b
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-21 12:56:51 +0100

    modified interceptor to redirect to login page on auth error

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,23 +1,23 @@
 export default function ({ $axios, store, redirect }) {
   // Automatically set bearer token for all request
   $axios.onRequest(() => {
-    $axios.setToken(store.state.user.token, "Bearer");
+    $axios.setToken(store.state.user.token, 'Bearer');
   });
 
   // If user make requests to endpoints (that is not /login or /register), refresh token
   $axios.onError((error) => {
     if (
       error.response.status === 401 &&
-      (error.request.responseURL.includes("login") ||
-        error.request.responseURL.includes("register"))
+      (error.request.responseURL.includes('login') ||
+        error.request.responseURL.includes('register'))
     ) {
       $axios
-        .post("/auth/refresh")
+        .post('/auth/refresh')
         .then((response) => {
           localStorage.token = response.token;
         })
         .catch((err) => {
-          console.log("token experied", error.request.responseURL);
+          redirect('/auth/login');
         });
     }
   });
