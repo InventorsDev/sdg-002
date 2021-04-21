be18bfb92f2a4d6bfad1e34b2da4c5acf2cf30a6
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-21 12:44:37 +0100

    added axios plugin to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/axios'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -67,4 +67,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};
