0c4a3da8cfd1b9082193f73a07b1bbddfce33c87
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-21 12:43:03 +0100

    added interceptor to automatically set request header & also refresh expired tokens

---
+++ plugins/axios.js
@@ -0,0 +1,24 @@
+export default function ({ $axios, store, redirect }) {
+  // Automatically set bearer token for all request
+  $axios.onRequest(() => {
+    $axios.setToken(store.state.user.token, "Bearer");
+  });
+
+  // If user make requests to endpoints (that is not /login or /register), refresh token
+  $axios.onError((error) => {
+    if (
+      error.response.status === 401 &&
+      (error.request.responseURL.includes("login") ||
+        error.request.responseURL.includes("register"))
+    ) {
+      $axios
+        .post("/auth/refresh")
+        .then((response) => {
+          localStorage.token = response.token;
+        })
+        .catch((err) => {
+          console.log("token experied", error.request.responseURL);
+        });
+    }
+  });
+}
