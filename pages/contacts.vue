ff9ad4d58fe3e36b0b3427ab182fc1e48764d78d
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-04-26 13:46:09 +0100

    added empty state to contacts list

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -4,49 +4,64 @@
       <h2 class="d-inline-block mt-5 text-green-400 font-weight-bolder">
         Life <br />savers
       </h2>
-      <section>
-        <h5 class="mt-5 text-green-400 fw-600">Recent</h5>
-        <div class="mt-4">
-          <!-- .... -->
-          <b-row
-            class="mt-3"
-            v-for="(contact, index) in recentContacts"
-            :key="index"
-          >
-            <b-col cols="auto">
-              <b-avatar
-                variant="info"
-                :text="contact.name[0]"
-                size="60px"
-              ></b-avatar>
-            </b-col>
-            <b-col cols="auto pl-0">
-              <h6 class="text-green-200 mt-2 mb-0">
-                {{ contact.name }}
-              </h6>
-              <p class="text-muted">
-                <small>{{ toRelativeTime(contact.created_at) }}</small>
-              </p>
+      <template v-if="favouriteContacts.length > 10">
+        <section>
+          <h5 class="mt-5 text-green-400 fw-600">Recent</h5>
+          <div class="mt-4">
+            <!-- .... -->
+            <b-row
+              class="mt-3"
+              v-for="(contact, index) in recentContacts"
+              :key="index"
+            >
+              <b-col cols="auto">
+                <b-avatar
+                  variant="info"
+                  :text="contact.name[0]"
+                  size="60px"
+                ></b-avatar>
+              </b-col>
+              <b-col cols="auto pl-0">
+                <h6 class="text-green-200 mt-2 mb-0">
+                  {{ contact.name }}
+                </h6>
+                <p class="text-muted">
+                  <small>{{ toRelativeTime(contact.created_at) }}</small>
+                </p>
+              </b-col>
+            </b-row>
+          </div>
+        </section>
+        <!-- ... -->
+        <section class="mt-5 mb-5">
+          <h5 class="text-green-400 d-inline-block fw-600">
+            List <span class="h5"><BIconChevronRight /></span>
+          </h5>
+
+          <b-row class="mt-4 flex-nowrap overflow-auto">
+            <b-col
+              cols="auto px-2"
+              v-for="(contact, index) in favouriteContacts"
+              :key="index"
+            >
+              <b-avatar class="border-0" size="90px"></b-avatar>
             </b-col>
           </b-row>
+        </section>
+      </template>
+      <template v-else>
+        <div class="mt-10vh text-center">
+          <img
+            src="~/assets/images/empty-contact.png"
+            class="img-fluid"
+            width="220"
+            alt="Empty Illustration"
+          />
+          <p class="text-muted px-2 mt-4">
+            Looks like you've not added any contact - yet.
+          </p>
         </div>
-      </section>
-      <!-- ... -->
-      <section class="mt-5 mb-5">
-        <h5 class="text-green-400 d-inline-block fw-600">
-          List <span class="h5"><BIconChevronRight /></span>
-        </h5>
-
-        <b-row class="mt-4 flex-nowrap overflow-auto">
-          <b-col
-            cols="auto px-2"
-            v-for="(contact, index) in favouriteContacts"
-            :key="index"
-          >
-            <b-avatar class="border-0" size="90px"></b-avatar>
-          </b-col>
-        </b-row>
-      </section>
+      </template>
       <div class="mt-20vh">&nbsp;</div>
     </div>
     <bottom-nav />
