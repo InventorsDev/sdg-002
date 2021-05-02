000b85ad9b49bc60e1bd3dc1c280b0140676da2d
Asaolu Elijah <asaoluelijah7@gmail.com>
2021-05-02 22:12:54 +0100

    added empty state

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -7,82 +7,95 @@
       </h2>
       <p class="text-muted fw-500">2 overdue notifications</p>
 
-      <!-- ... -->
-      <section class="mt-5 mb-5">
-        <h5 class="text-green-400 d-inline-block fw-600">Upcoming</h5>
-        <!-- .. -->
-        <div class="px-3">
-          <b-row
-            class="mt-4 p-3 bg-light rounded"
-            v-for="(reminder, index) in reminders.upcoming"
-            :key="index"
-          >
-            <b-col cols="auto pr-0">
-              <b-form-checkbox
-                size="lg"
-                :checked="reminder.data.completed"
-                @change="markAsComplete(reminder.id)"
-              ></b-form-checkbox>
-            </b-col>
-            <b-col cols="auto">
-              <h6 class="text-green-400 mb-0">
-                {{ reminder.data.drug_name }} -
-                {{ reminder.data.medication_id + ' Tablets' }}
-              </h6>
-              <small class="d-inline-block mt-2 text-muted">
-                To be taken {{ toRelativeTime(reminder.data.to_be_taken_at) }}
-              </small>
-            </b-col>
-          </b-row>
-        </div>
-        <!-- .. -->
-      </section>
-
-      <section>
-        <h5 class="mt-5 text-green-400 fw-600">Past</h5>
-        <div class="mt-4 px-3">
+      <template v-if="reminders.length > 0">
+        <!-- ... -->
+        <section class="mt-5 mb-5">
+          <h5 class="text-green-400 d-inline-block fw-600">Upcoming</h5>
           <!-- .. -->
-          <b-row
-            class="mt-4"
-            v-for="(reminder, index) in reminders.past"
-            :key="index"
-          >
-            <b-col cols="auto pr-0">
-              <b-form-checkbox size="lg" :checked="true"></b-form-checkbox>
-            </b-col>
-            <b-col cols="auto">
-              <h6 class="text-green-400 mb-0">
-                {{ reminder.data.medication_id + ' Tablets' }} of
-                {{ reminder.data.drug_name }}
-              </h6>
-              <small class="text-muted">
-                Completed {{ toRelativeTime(reminder.updated_at) }}
-              </small>
-            </b-col>
-          </b-row>
+          <div class="px-3">
+            <b-row
+              class="mt-4 p-3 bg-light rounded"
+              v-for="(reminder, index) in reminders.upcoming"
+              :key="index"
+            >
+              <b-col cols="auto pr-0">
+                <b-form-checkbox
+                  size="lg"
+                  :checked="reminder.data.completed"
+                  @change="markAsComplete(reminder.id)"
+                ></b-form-checkbox>
+              </b-col>
+              <b-col cols="auto">
+                <h6 class="text-green-400 mb-0">
+                  {{ reminder.data.drug_name }} -
+                  {{ reminder.data.medication_id + ' Tablets' }}
+                </h6>
+                <small class="d-inline-block mt-2 text-muted">
+                  To be taken {{ toRelativeTime(reminder.data.to_be_taken_at) }}
+                </small>
+              </b-col>
+            </b-row>
+          </div>
+          <!-- .. -->
+        </section>
+
+        <section>
+          <h5 class="mt-5 text-green-400 fw-600">Past</h5>
+          <div class="mt-4 px-3">
+            <!-- .. -->
+            <b-row
+              class="mt-4"
+              v-for="(reminder, index) in reminders.past"
+              :key="index"
+            >
+              <b-col cols="auto pr-0">
+                <b-form-checkbox size="lg" :checked="true"></b-form-checkbox>
+              </b-col>
+              <b-col cols="auto">
+                <h6 class="text-green-400 mb-0">
+                  {{ reminder.data.medication_id + ' Tablets' }} of
+                  {{ reminder.data.drug_name }}
+                </h6>
+                <small class="text-muted">
+                  Completed {{ toRelativeTime(reminder.updated_at) }}
+                </small>
+              </b-col>
+            </b-row>
+          </div>
+        </section>
+        <!-- ... -->
+        <section class="mt-5 mb-5">
+          <h5 class="text-green-400 d-inline-block fw-600">Daily tips</h5>
+          <!-- .. -->
+          <div>
+            <b-card
+              overlay
+              img-src="~/assets/images/tip-card.png"
+              img-alt="Card Image"
+              text-variant="green-400"
+              class="border-0 p-auto"
+            >
+              <h5>
+                An apple a day, <br />
+                keeps the doctor <br />
+                away.
+              </h5>
+            </b-card>
+          </div>
+          <!-- .. -->
+        </section>
+      </template>
+      <template v-else>
+        <div class="text-center mt-20vh">
+          <img
+            src="~/assets/images/calender.png"
+            class="img-fluid"
+            width="150"
+            alt=""
+          />
+          <p class="px-3 text-muted mt-4">Opps! you've not created any reminder.</p>
         </div>
-      </section>
-      <!-- ... -->
-      <section class="mt-5 mb-5">
-        <h5 class="text-green-400 d-inline-block fw-600">Daily tips</h5>
-        <!-- .. -->
-        <div>
-          <b-card
-            overlay
-            img-src="~/assets/images/tip-card.png"
-            img-alt="Card Image"
-            text-variant="green-400"
-            class="border-0 p-auto"
-          >
-            <h5>
-              An apple a day, <br />
-              keeps the doctor <br />
-              away.
-            </h5>
-          </b-card>
-        </div>
-        <!-- .. -->
-      </section>
+      </template>
       <div class="mt-20vh">&nbsp;</div>
     </div>
     <bottom-nav />
